f415f8902153ecfc80603a707626b10651a4754e
malwamaba@gmail.com <malwamaba@gmail.com>
2023-12-12 19:31:56 +0200

    Added new components for dashboard, login, register, send-crypto, and transaction-history

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SendCryptoComponent } from './components/send-crypto/send-crypto.component';
+import { TransactionHistoryComponent } from './components/transaction-history/transaction-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    SendCryptoComponent,
+    TransactionHistoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/send-crypto/send-crypto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-send-crypto',
+  templateUrl: './send-crypto.component.html',
+  styleUrls: ['./send-crypto.component.scss']
+})
+export class SendCryptoComponent {
+
+}
