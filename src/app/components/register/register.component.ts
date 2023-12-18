9af1e4f7c716004fd174ca825178a3d507e00c40
malwamaba@gmail.com <malwamaba@gmail.com>
2023-12-18 11:31:54 +0200

    Modified register component...

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  constructor(private authService: AuthService) { }
+
+  onSubmit() {
+    this.authService.register(
+      {
+        FirstName: this.firstName,
+        LastName: this.lastName,
+        Email: this.email,
+        Password: this.password,
+        ConfirmPassword: this.confirmPassword
+      }).subscribe(response => { console.log(response) });
+  }
 
 }
