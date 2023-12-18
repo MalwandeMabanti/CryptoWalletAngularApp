48cf3de23bdfc55f1e6545f1c4fecc8014b7d7c3
malwamaba@gmail.com <malwamaba@gmail.com>
2023-12-18 11:29:07 +0200

    Modified dashboard component...

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  userName: string = "";
+  balance: number = 0;
+  transactions: any[] = []; 
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.userName = 'John Doe';
+    this.balance = 1000; 
+    this.loadTransactions();
+  }
+
+  loadTransactions(): void {
+
+    this.transactions = [];
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate([""]);
+  }
+
+  refreshData(): void {
+    this.loadTransactions();
+  }
 
 }
