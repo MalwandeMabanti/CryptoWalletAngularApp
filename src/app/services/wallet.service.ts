0970ddc925d08c2da07e836b5113bd30f80d3966
malwamaba@gmail.com <malwamaba@gmail.com>
2023-12-18 11:24:41 +0200

    Modifiesd wallet service...

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Wallet } from '../models/wallet.model';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class WalletService {
 
-  constructor() { }
+  private apiUrl = 'https://localhost:44370/api/';
+
+  constructor(private http: HttpClient) { }
+
+  sendCrypto(data: Wallet)
+  {
+    return this.http.post(`${this.apiUrl}`+"wallet/send", data)
+  }
+
+
+
 }
