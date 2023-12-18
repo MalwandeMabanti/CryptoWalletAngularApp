d495d95dd5c729cff321e0201fdfde44dd12b2dd
malwamaba@gmail.com <malwamaba@gmail.com>
2023-12-18 11:29:51 +0200

    Modified login component...

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service'
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  onSubmit() {
+    this.authService.login(
+      {
+        Email: this.email,
+        Password: this.password
+
+      }).subscribe(
+        {
+          next: (response) => {
+            if (response.token) {
+              this.router.navigate(['/dashboard']);
+            }
+            else {
+              console.error("No token received");
+            }
+          },
+          error: (error) => {
+            console.error("Login error", error)
+          }
+        });
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
 
 }
