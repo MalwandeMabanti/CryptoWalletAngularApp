021e35a1f3e5f90565c330f1fafded3bee84c657
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-10 21:03:29 +0200

    Made changes to the dashboard component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Transaction } from '../../models/transaction.model';
 import { AuthService } from '../../services/auth.service';
+import { WalletService } from '../../services/wallet.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,24 +11,68 @@ import { AuthService } from '../../services/auth.service';
 })
 export class DashboardComponent implements OnInit {
 
-  userName: string = "";
+  userId: string = "";
+  firstName: string = "";
+  lastName: string = "";
+  email: string = "";
   balance: number = 0;
-  transactions: any[] = []; 
+  transactions: Transaction[] = []; 
 
   constructor(
     private authService: AuthService,
+    private walletService: WalletService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.userName = 'John Doe';
-    this.balance = 1000; 
-    this.loadTransactions();
+    this.getUserWithTransactions();
+  }
+
+  getUserWithTransactions()
+  {
+    this.walletService.getSpecificUserWithTransactions()
+      .subscribe(
+        {
+          next: (response) => {
+
+            console.log("Response:", response);
+
+            this.userId = response.userId
+            this.firstName = response.firstName
+            this.lastName = response.lastName
+            this.email = response.email
+            this.balance = response.balance
+            this.transactions = response.transactions
+
+            console.log("Assigned Values:", {
+              userId: this.userId,
+              firstName: this.firstName,
+              lastName: this.lastName,
+              email: this.email,
+              balance: this.balance,
+              transactions: this.transactions
+            });
+
+          },
+          error: (err) => {
+            console.error('Error fetching user with transactions:', err);
+          }
+        })
   }
 
   loadTransactions(): void {
 
-    this.transactions = [];
+    this.walletService.receiveAllTransactions()
+      .subscribe(
+        {
+          next: (response) => {
+            this.transactions = response
+          },
+          error: (error) => {
+            console.error(error)
+          }
+      }
+    );
   }
 
   logout(): void {
