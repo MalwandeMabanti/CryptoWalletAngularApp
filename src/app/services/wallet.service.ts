48159eccba8cc17815ef653a1df580823679d3c2
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-10 21:11:34 +0200

    Made some changes to the Wallet service

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Wallet } from '../models/wallet.model';
 import { HttpClient } from '@angular/common/http'
+import { Transaction } from '../models/transaction.model';
+import { SpecificUser } from '../models/specific-user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WalletService {
 
-  private apiUrl = 'https://localhost:44370/api/';
+  private apiUrl = 'https://localhost:44370/api/wallet/';
 
   constructor(private http: HttpClient) { }
 
   sendCrypto(data: Wallet)
   {
-    return this.http.post(`${this.apiUrl}`+"wallet/send", data)
+    return this.http.post(`${this.apiUrl}`+"send", data)
+  }
+
+  receiveAllTransactions()
+  {
+    return this.http.get<Transaction[]>(`${this.apiUrl}"GetAllUserTransactions"`)
+  }
+
+  getSpecificUserWithTransactions()
+  {
+    return this.http.get<SpecificUser>(`${this.apiUrl}GetSpecificUserWithTransactions`)
   }
 
 
