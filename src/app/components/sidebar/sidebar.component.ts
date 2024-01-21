78ddceb9e5d8cddfca61a4e39a30316031b93e27
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-21 21:06:54 +0200

    Added sideBar component.

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  goToBitcoin()
+  {
+    this.router.navigate(['/cryptoInformation'])
+  }
+}
