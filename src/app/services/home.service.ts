07ed44435dc821fb29e908635ed554b699e397b0
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-21 21:13:51 +0200

    Made changes in the home service.

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -7,7 +7,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class HomeService {
 
-  //private apiUrl = "https://api.coingecko.com/api/v3/coins/";
   private url = 'https://bitcoinaverage-crypto-ticker-and-historical-price.p.rapidapi.com/indices/global/ticker/BTCUSD';
   constructor(private http: HttpClient)
   {
@@ -22,12 +21,4 @@ export class HomeService {
 
     return this.http.get<Home>(this.url, { headers });
   }
-
-  //getEthereumInformation() {
-  //  return this.http.get<Home[]>(`${this.apiUrl}markets?vs_currency=usd&ids=ethereum&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1&locale=en&precision=3`);
-  //}
-
-  //getBinanceUSDInformation() {
-  //  return this.http.get<Home[]>(`${this.apiUrl}markets?vs_currency=usd&ids=binance-usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1&locale=en&precision=3`);
-  //}
 }
