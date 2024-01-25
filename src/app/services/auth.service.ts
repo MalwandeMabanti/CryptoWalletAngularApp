515dae2092d59c3939126cfe172a7aaa8bd974e2
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-25 22:30:19 +0200

    Made changes to authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { AuthResponse } from '../models/auth-response.model';
 import { Login } from '../models/login.model';
 import { Register } from '../models/register.model';
@@ -11,6 +11,8 @@ import { Register } from '../models/register.model';
 export class AuthService {
 
   private apiUrl = 'https://localhost:44370/api/auth/';
+  private isLoggedInSubject = new BehaviorSubject<boolean>(this.hasToken());
+  public isLoggedIn$ = this.isLoggedInSubject.asObservable();
 
   private post<T>(endpoint: string, payload: any)
   {
@@ -22,12 +24,16 @@ export class AuthService {
 
   private handleAuthResponse(response$: Observable<AuthResponse>): Observable<AuthResponse> {
     return response$.pipe(
-      tap(response => this.setToken(response.token))
+      tap(response => {
+        this.setToken(response.token);
+        this.isLoggedInSubject.next(true);
+      })
     );
   }
 
   login(loginData: Login): Observable<AuthResponse> {
     const response$ = this.post<AuthResponse>("login", loginData);
+   
     return this.handleAuthResponse(response$);
   }
 
@@ -38,18 +44,28 @@ export class AuthService {
 
   logout(): void {
     this.removeToken();
+    this.isLoggedInSubject.next(false);
   }
 
 
-  getToken(): string | null {
+   getToken(): string | null {
     return localStorage.getItem('authToken');
   }
 
-  setToken(token: string): void {
+  private setToken(token: string): void {
     localStorage.setItem('authToken', token);
   }
 
   removeToken(): void {
     localStorage.removeItem('authToken');
   }
+
+  private hasToken(): boolean {
+    return this.getToken() !== null;
+  }
+
+  //isLoggedIn(): boolean
+  //{
+  //  return this.getToken() !== null;
+  //}
 }
