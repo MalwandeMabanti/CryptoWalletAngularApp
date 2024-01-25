4a47d8f2cd21f64bb15397e1ee8dc590e6f4472f
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-25 22:32:39 +0200

    Made changes to navBar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router} from '@angular/router';
+import { filter } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+    isLoggedIn: boolean = false;
+
+    constructor(
+      private router: Router,
+      private authService: AuthService
+    )
+    {
+    }
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe(loggedIn => {
+      this.isLoggedIn = loggedIn;
+    })
+
+  }
+
+    login(): void {
+     
+      this.router.navigate(["/login"]);
+      
+    }
+
+
+
+
+    logout(): void {
+      this.authService.logout();
+      this.router.navigate([""]);
+    }
+
+  get currentUrl() {
+    return this.router.url;
+  }
+
+  get homeUrl()
+  {
+    return this.router.url;
+  }
+
 
 }
