20286e1b1a9c54601d8c0ffccef0eda360c08159
malwamaba@gmail.com <malwamaba@gmail.com>
2024-01-28 23:38:19 +0200

    Modified the home component.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { PublicTrasactions } from '../../models/public-transaction.model';
+import { WalletService } from '../../services/wallet.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit
+{
+
+    allTransactions: PublicTrasactions[] = [];
+
+    constructor(private walletService: WalletService)
+    {
+    }
+
+    ngOnInit(): void {
+      this.receiveAllTransactions()
+    }
+
+  receiveAllTransactions(): void
+  {
+    this.walletService.receiveAllTransactions()
+      .subscribe(
+        {
+          next: (response) => {
+            this.allTransactions = response
+            this.allTransactions.forEach(element =>
+            {
+              console.log(this.allTransactions, " and everything else")
+            })
+          },
+          error: (err) => {
+            console.error('Error fetching user with transactions:', err);
+          }
+        }
+      )
+  }
+
+
+}
 
